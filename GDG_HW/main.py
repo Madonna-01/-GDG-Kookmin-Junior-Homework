af3e13efcd6800babd9e33105182f163febe6898
Madonna-01 <kms750725@naver.com>
2024-09-30 19:08:50 +0900

    First homework

---
+++ GDG_HW/main.py
@@ -0,0 +1,8 @@
+def generate_hello_world_str() :
+    return str(input("문자열 입력 : "))
+
+def print_console() :
+    print(generate_hello_world_str())
+
+if __name__ == '__main__':
+    print_console()
